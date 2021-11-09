f41c0c645ebf00eebb528ea7719f3b9a728975fd
removable <me@imguan.com>
2021-11-09 18:17:23 +0800

    临时提交

--- UnoGameBackend/Hubs/UnoHub.cs
+++ UnoGameBackend/Hubs/UnoHub.cs
@@ -1,18 +1,56 @@
-﻿using Microsoft.AspNetCore.SignalR;
+﻿using System.Collections.Concurrent;
+using Microsoft.AspNetCore.SignalR;
 using UnoGameBackend.Data;
 
 namespace UnoGameBackend.Hubs
 {
     public class UnoHub : Hub
     {
+        /// <summary>
+        /// username与SignalR的connectionId的对应关系
+        /// </summary>
+        private static ConcurrentDictionary<string, HashSet<string>> UsernameToSignalRId = new();
+
         public override Task OnDisconnectedAsync(Exception? exception)
         {
+            //TODO live room
             return base.OnDisconnectedAsync(exception);
         }
 
         public override Task OnConnectedAsync()
         {
             return base.OnConnectedAsync();
+        }
+
+        public async Task UserLogout(string username, int roomId = -1)
+        {
+            await LogoutBase(username, roomId);
+        }
+
+        private async Task LogoutBase(string username, int roomId = -1)
+        {
+            try
+            {
+                username = username.Trim();
+                if (!Player.Players.ContainsKey(username.ToLower()))
+                    throw new Exception("找不到用户");
+                var user = Player.Players[username];
+                if (roomId >= 0)
+                {
+                    var room = Room.Rooms.FirstOrDefault(r => r.Id == 0);
+                    if (room == null)
+                        throw new Exception("找不到房间");
+
+                    var userIndex = Array.IndexOf(room.Players, user);
+                    room.Players[userIndex] = null;
+                    await UpdateRoomPlayers(room);
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                await Clients.Group(username.ToLower()).SendAsync("LogoutResult", false, e.Message);
+            }
         }
 
         private async Task UpdateRoomPlayers(Room room)
@@ -33,6 +71,8 @@ namespace UnoGameBackend.Hubs
             try
             {
                 username = username.Trim();
+                if (string.IsNullOrWhiteSpace(username))
+                    throw new Exception("昵称不能为空");
                 var user = new Player();
                 if (Player.Players.ContainsKey(username.ToLower()))
                 {
@@ -62,7 +102,8 @@ namespace UnoGameBackend.Hubs
         {
             try
             {
-                if (!Player.Players.ContainsKey(username.ToLower()))
+                var lowerUsername = username.ToLower();
+                if (!Player.Players.ContainsKey(lowerUsername))
                     throw new Exception("找不到用户");
                 var user = Player.Players[username.ToLower()];
                 var room = Room.Rooms.FirstOrDefault(i => i.Id == roomId);
@@ -72,9 +113,23 @@ namespace UnoGameBackend.Hubs
                 if ((room.Game?.Status ?? GameStatus.Waiting) == GameStatus.Playing)
                     throw new Exception("当前房间已开始游戏");
 
-                room.Players[Array.IndexOf(room.Players, null)] = user;
+                if (!room.Players.Any(p =>
+                        string.Equals(p?.Username, username, StringComparison.CurrentCultureIgnoreCase)))
+                {
+                    room.Players[Array.IndexOf(room.Players, null)] = user;
+                }
+
                 await Groups.AddToGroupAsync(Context.ConnectionId, $"game-{roomId.ToString()}");
                 await UpdateRoomPlayers(room);
+                await Clients.Group(username.ToLower()).SendAsync("JoinRoomResult", true, "");
+                var hs = new HashSet<string>();
+                if (UsernameToSignalRId.ContainsKey(username))
+                {
+                    hs = UsernameToSignalRId[username];
+                }
+
+                hs.Add(Context.ConnectionId);
+                UsernameToSignalRId.AddOrUpdate(username, hs, (k, oldValue) => hs);
             }
             catch (Exception e)
             {
